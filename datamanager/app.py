0bbebffaacaddeb90c94878e5d271df0412c88f8
billbern <denis.abaidoo@gmail.com>
2024-05-15 11:11:46 +0000

    secured environment variable

--- datamanager/app.py
+++ datamanager/app.py
@@ -1,4 +1,4 @@
-import logging
+import os, logging
 from functools import wraps
 from telegram import Update
 from telegram.ext import CommandHandler, Application, ContextTypes
@@ -9,9 +9,9 @@ logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 def restricted(func):
     @wraps(func)
-    async def wrapped(update: Update, context: ContextTypes.DEFAULT_TYPE, *args, **kwargs) -> None:
+    def wrapped(update: Update, context: ContextTypes.DEFAULT_TYPE, *args, **kwargs) -> None:
         if update.message.chat.type != "private" and update.effective_chat.username != "BernardAbaidoo":
-            return await context.bot.send_message(chat_id=update.effective_chat.id, text="you are not allowed to send this message")
+            return context.bot.send_message(chat_id=update.effective_chat.id, text="you are not allowed to send this message")
         return func(update, context, *args, **kwargs)
     return wrapped
 
@@ -21,9 +21,14 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 @restricted
 async def get_message_files(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    bot_data = await context.bot.get_updates()
-    print(bot_data)
+    user_id = update.message.from_user.id
+    print("\n\n", update, "\n\n")
     command, messages = update.message.text.split(maxsplit=1)
+    
+    groups = []
+    # user_groups = await context.bot.get_chat_member(user_id=user_id)
+    # print("\n\n", user_groups, "\n\n")
+    
     
     if "payment" in messages and "issue" in messages:
         # TODO: get all documents with 'payment' and 'issue' in document name
@@ -32,9 +37,14 @@ async def get_message_files(update: Update, context: ContextTypes.DEFAULT_TYPE) 
         # TODO: get all documents with 'payment' and 'report' in document name
         pass
     
+    if len(groups) > 0:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{groups[0]}")
+    else:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="No groups found")
+    
 
 def main() -> None:
-    application = Application.builder().token("6735004489:AAFOV9Bq0LaGmIgKtU6Aoaji2MxIjZmHME0").build()
+    application = Application.builder().token(os.environ.get('TELEGRAM_BOT_TOKEN')).build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("compile", get_message_files))
     application.run_polling(allowed_updates=Update.ALL_TYPES)
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from datamanager.app import main
 
 
