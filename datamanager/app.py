56f441b599721d249aa3806ab2a4cbdd65ffbac0
billbern <denis.abaidoo@gmail.com>
2024-05-15 04:57:02 +0000

    added a restriction decorator

--- datamanager/app.py
+++ datamanager/app.py
@@ -1,5 +1,5 @@
 import logging
-
+from functools import wraps
 from telegram import Update
 from telegram.ext import CommandHandler, Application, ContextTypes
 
@@ -7,16 +7,23 @@ from telegram.ext import CommandHandler, Application, ContextTypes
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
 
+def restricted(func):
+    @wraps(func)
+    async def wrapped(update: Update, context: ContextTypes.DEFAULT_TYPE, *args, **kwargs) -> None:
+        if update.message.chat.type != "private" and update.effective_chat.username != "BernardAbaidoo":
+            return await context.bot.send_message(chat_id=update.effective_chat.id, text="you are not allowed to send this message")
+        return func(update, context, *args, **kwargs)
+    return wrapped
+
+@restricted
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if context:
-        pass
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello i am the AFK Data Manager, how can i held you!")
 
+@restricted
 async def get_message_files(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     bot_data = await context.bot.get_updates()
     print(bot_data)
     command, messages = update.message.text.split(maxsplit=1)
-    
     
     if "payment" in messages and "issue" in messages:
         # TODO: get all documents with 'payment' and 'issue' in document name
